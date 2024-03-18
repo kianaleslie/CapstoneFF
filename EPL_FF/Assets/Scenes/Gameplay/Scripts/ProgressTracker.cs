575db54e016b83a640f0f6bdf3b0d78cbab36a4d
kiana <kianaleslie25@gmail.com>
2024-03-18 15:47:31 -0600

    trying to update the sliderbar to mirror player selection

--- EPL_FF/Assets/Scenes/Gameplay/Scripts/ProgressTracker.cs
+++ EPL_FF/Assets/Scenes/Gameplay/Scripts/ProgressTracker.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UIElements;
 using UnityEngine.UI;
 
 public class ProgressTracker : MonoBehaviour
 {
     [SerializeField] GameObject endpoint, currentPosition;
-    [SerializeField] UnityEngine.UI.Slider progressSlider;
+    [SerializeField] Slider progressSlider;
+    [SerializeField] GameObject handleImage;
+    [SerializeField] GameObject[] updateSnowballImages;
+     GlobalDataManager globalDataManager;
     float trackerValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        globalDataManager = FindObjectOfType<GlobalDataManager>();
+        handleImage.GetComponent<RawImage>().texture = updateSnowballImages[globalDataManager.selectedSnowballID].gameObject.GetComponent<RawImage>().texture;
     }
 
     // Update is called once per frame
@@ -25,5 +28,15 @@ public class ProgressTracker : MonoBehaviour
         trackerValue = trackerValue * 100;
 
         progressSlider.value = trackerValue;
+
+        //handleImage.GetComponent<RawImage>().texture = updateSnowballImages[globalDataManager.selectedSnowballID].gameObject.GetComponent<RawImage>().texture;
+
+        //for (int i = 0; i < updateSnowballImages.Length; i++)
+        //{
+        //    if (globalDataManager.selectedSnowballID == 0)
+        //    {
+        //        updateSnowballImages[0].gameObject.SetActive(true);
+        //    }
+        //}
     }
 }
